5331d7b0adc8c5cfa6e52c6a0ed63fce7b99d32b
mgarkel <manav.garkel@gmail.com>
2020-09-03 20:32:44 -0400

    updated thumbnails in campground index

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var campGround = require("./models/campgrounds");
